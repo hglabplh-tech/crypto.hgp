f8bf09fdb4976c3dc54e70b48a77024f1ae22abc
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-01-17 21:46:41 +0100

    save work one error in compile

---
+++ cryptoasn1/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+
+;; pkg info
+
+(define version "1.0")
+(define collection "cryptoasn1")
+(define deps '("base" "crypto-asn1"))
+(define implies '("crypto-asn1"))
+(define pkg-authors '(ryanc hglabplh))
+
+;; collection info
+
+(define name "cryptoasn1")
