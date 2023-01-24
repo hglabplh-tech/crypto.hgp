1e2ad86c49e01269861c12367deffe7114d55765
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-01-25 00:06:18 +0100

    added missing accidentally discarded files

---
+++ crypto-doc/info.rkt
@@ -0,0 +1,21 @@
+#lang info
+
+;; pkg info
+
+(define version "1.0")
+(define collection "crypto")
+(define deps '("base"))
+(define build-deps '("scramble-lib"
+                     "scramble"
+                     "racket-doc"
+                     "scribble-lib"
+                     "crypto-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "crypto")
+(define scribblings '(("scribblings/crypto.scrbl" (multi-page))))
+
+(define compile-omit-paths '("examples"))
+(define test-omit-paths '("examples"))
