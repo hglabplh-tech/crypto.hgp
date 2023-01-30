1bfcd03fe42b4ee34c5b46a52be5e939065c467d
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-01-30 11:52:14 +0100

    commit a batch script to generate certificates and keys for testing

---
+++ crypto-lib/private/libcrypto/data/openssl-chaincreate.bat
@@ -0,0 +1,9 @@
+openssl req -new -newkey rsa:2048 -nodes -out ca.csr -set_serial 05 -keyout ca.key
+openssl x509 -trustout -signkey ca.key -days 365 -req -set_serial 05 -in ca.csr -out ca.pem
+openssl genrsa -out client.key 2048
+openssl req -new -key client.key -out client.csr
+openssl x509 -req -in client.csr -days 1000 -CA ca.pem -CAkey ca.key -set_serial 05 > client.cer
+openssl rsa -inform PEM -outform DER -in ca.key -out freeware-ca-key.der
+openssl rsa -inform PEM -outform DER -in client.key -out freeware-user-key.der
+openssl x509 -inform PEM -outform DER -in client.cer -out freeware-user-cert.der
+openssl x509 -inform PEM -outform DER -in ca.pem  -out freeware-ca-cert.der
