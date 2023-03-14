6e92d38fca3706f531d2f5ec1e628fd0253e4b3f
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-03-14 22:51:01 +0100

    added export tests to examples/cmssig-example.rkt

--- crypto-doc/examples/cmssig-example.rkt
+++ crypto-doc/examples/cmssig-example.rkt
@@ -3,6 +3,12 @@
 
 (crypto-factories libcrypto-factory)
 
+(define cipher-name "AES-256-CBC")
+
+(define symetric-key (get-symmetric-key cipher-name))
+
+;; up to now only check if the entry is really exported 
 (cms-sign-simple "")
 (cms-init-signing "")
 (cms-add-signing-cert "")
+(write-CMS/BER "")
