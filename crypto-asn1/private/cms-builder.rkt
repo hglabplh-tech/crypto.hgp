05da4e27b3082989914edfffbae94baacc96f97d
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-04-24 23:57:56 +0200

    added real builder class-definitions for generating cms signed data

--- crypto-asn1/private/cms-builder.rkt
+++ crypto-asn1/private/cms-builder.rkt
@@ -115,10 +115,14 @@
   (make-cert-val-getter
    cert-bytes))
 
-(define (build-certificate-set cert-bytes)
+(define (build-certificate-set cert-bytes-list)
+  (let ([cert-set (map build-certificate cert-bytes-list)])
+    cert-set))
+
+(define (build-certificate cert-bytes)
   (let ([cert-asn1 (cert->asn1/DER cert-bytes)])
-    (make-set-of (check-and-make-choice certificate-choice
-                                        (list 'certificate cert-asn1)))))
+    (check-and-make-choice certificate-choice
+                           (list 'certificate cert-asn1))))
 ;; in the following we have to introduce lambda for signature and digest taking the neccessary arguments
 ;; data and algorithm....
 (define (build-signer-info cert-bytes digest-alg private-key
@@ -141,35 +145,290 @@
 
 
 
-(define (build-signed-data digest-alg content-bytes cert-bytes crls signer-infos) 
+(define (build-signed-data digest-algs content-bytes cert-bytes-list other-x509certs attr-certs crls priv-keys
+                           calc-digest-proc
+                           sign-digest-proc) 
   (check-and-make-sequence signed-data-seq 
-                           (list 1
-                                 (make-set-of digest-alg)
+                           (list (cond [(not (null? attr-certs)) 4][else 1]) ;; version
+                                 digest-algs
                                  (check-and-make-sequence
                                   encap-content-info-seq (list id-cms-data content-bytes))
-                                 (cond [cert-bytes ;; complete this to real sequence
-                                        (build-certificate-set cert-bytes)]
-                                       [else cert-bytes])
-                                 crls signer-infos)))
+                                 (build-certificate-set (append cert-bytes-list other-x509certs))                                    
+                                 crls (build-sig-infos digest-algs content-bytes cert-bytes-list priv-keys
+                                                       calc-digest-proc
+                                                       sign-digest-proc))))
+
+(define (build-sig-infos digest-algs content-bytes cert-bytes-list priv-keys
+                         calc-digest-proc
+                         sign-digest-proc)
+  (let recur-signerinf ([sig-infos null]
+                        [int-cert-list cert-bytes-list]
+                        [int-digest-algs digest-algs]
+                        [int-priv-keys priv-keys])
+    (cond [(not (or (null? int-cert-list)
+                    (null? int-digest-algs)
+                    (null? int-priv-keys)))
+           (recur-signerinf (append sig-infos
+                                    (list
+                                     (build-signer-info (car int-cert-list)
+                                                        (car int-digest-algs) 'priv-key
+                                                        calc-digest-proc
+                                                        sign-digest-proc
+                                                        content-bytes)))
+                            (cdr int-cert-list)
+                            (cdr int-digest-algs)
+                            (cdr int-priv-keys))]
+          [else sig-infos])))
+                                               
+           
+
+  
+                               
 
 (define (build-cms-content type content)
   (check-and-make-sequence content-info-seq (list type content)))
 
+;; the classes this will be the truth
+
+(define signed-data-builder%
+  (class object%
+    (super-new)
+    (init-field [digest-algs  null]
+                [content-bytes #f]
+                [cert-bytes-list null]
+                [other-x509certs null]
+                [attr-certs null]
+                [crls null]               
+                [signer-infos null])
+    
+    (define/public (add-digest-alg algorithm)
+      (new this% [digest-algs (append digest-algs (list algorithm))]
+           [content-bytes content-bytes]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs other-x509certs]
+           [attr-certs attr-certs]
+           [crls crls]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-content-bytes content)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs other-x509certs]
+           [attr-certs attr-certs]
+           [crls crls]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-sig-bytes sig-bytes)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content-bytes]
+           [cert-bytes-list (append cert-bytes-list (list sig-bytes))]
+           [other-x509certs other-x509certs]
+           [attr-certs attr-certs]
+           [crls crls]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-more-certs more-certs)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content-bytes]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs more-certs]
+           [attr-certs attr-certs]
+           [crls crls]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-attr-certs attribute-certs)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content-bytes]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs other-x509certs]
+           [attr-certs attribute-certs]
+           [crls crls]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-crls cert-revoc-lists)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content-bytes]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs other-x509certs]
+           [attr-certs attr-certs]
+           [crls cert-revoc-lists]
+           [signer-infos signer-infos]
+           ))
+
+    (define/public (add-to-sig-infos-int signer-info)
+      (new this% [digest-algs digest-algs]
+           [content-bytes content-bytes]
+           [cert-bytes-list cert-bytes-list]
+           [other-x509certs other-x509certs]
+           [attr-certs attr-certs]
+           [crls crls]
+           [signer-infos (append signer-infos (list signer-info))]
+           )) 
+
+  
+    
+    (define/public (add-signer-info)
+      (new signer-info% [content-bytes content-bytes] [parent-obj this]))
+
+    (define/public (build-asn1)
+      (check-and-make-sequence signed-data-seq 
+                               (list (cond [(not (null? attr-certs)) 4][else 1]) ;; version
+                                     digest-algs
+                                     (check-and-make-sequence
+                                      encap-content-info-seq (list id-cms-data content-bytes))
+                                     (build-certificate-set (append cert-bytes-list other-x509certs))                                    
+                                     crls (map (lambda (sig-info)
+                                                 (send sig-info build-asn1)) signer-infos))))
+    
+    ))
+
+(define signer-info%
+  (class object%
+    (super-new)
+    (init-field [cert-bytes #f]
+                [digest-alg #f]
+                [private-key #f]
+                [calc-digest-proc #f]
+                [sign-digest-proc #f]
+                [content-bytes #f]
+                [parent-obj #f])
+    
+    (define/public (add-cert-bytes cert-bytes-in)
+      (new this%
+           [digest-alg digest-alg]
+           [content-bytes content-bytes]
+           [cert-bytes cert-bytes-in]         
+           [private-key private-key]
+           [calc-digest-proc calc-digest-proc]
+           [sign-digest-proc sign-digest-proc]
+           [parent-obj (send parent-obj add-sig-bytes cert-bytes-in)]
+           ))
+
+    (define/public (add-digest-alg algorithm)
+      (new this%
+           [digest-alg algorithm]
+           [content-bytes content-bytes]
+           [cert-bytes cert-bytes]         
+           [private-key private-key]
+           [calc-digest-proc calc-digest-proc]
+           [sign-digest-proc sign-digest-proc]
+           [parent-obj (send parent-obj add-digest-alg algorithm)]
+           ))
+
+    (define/public (add-private-key private-key-in)
+      (new this%
+           [digest-alg digest-alg]
+           [content-bytes content-bytes]
+           [cert-bytes cert-bytes]         
+           [private-key private-key-in]
+           [calc-digest-proc calc-digest-proc]
+           [sign-digest-proc sign-digest-proc]
+           [parent-obj parent-obj]
+           ))
+
+    (define/public (add-calc-digest-proc c-dig-proc)
+      (new this%
+           [digest-alg digest-alg]
+           [content-bytes content-bytes]
+           [cert-bytes cert-bytes]         
+           [private-key private-key]
+           [calc-digest-proc c-dig-proc]
+           [sign-digest-proc sign-digest-proc]
+           [parent-obj parent-obj]
+           ))
+
+    (define/public (add-sign-digest-proc sign-dig-proc)
+      (new this%
+           [digest-alg digest-alg]
+           [content-bytes content-bytes]
+           [cert-bytes cert-bytes]         
+           [private-key private-key]
+           [calc-digest-proc calc-digest-proc]
+           [sign-digest-proc sign-dig-proc]
+           [parent-obj parent-obj]
+           ))
+    
+    (define/public (end-def)
+      (send parent-obj add-to-sig-infos-int this))
+      
+    
+    ;; add the missing setters / add content / add parent set and end
+    (define/public (build-asn1)
+      (build-signer-info cert-bytes digest-alg private-key
+                         calc-digest-proc
+                         sign-digest-proc content-bytes))
+    
+
+    ))
+
+
+
+;; the tests
+
 (let ([content-data  (build-cms-content
                       id-cms-signed-data
-
                       (build-signed-data
-                       (build-alg-id id-sha512 #f)
+                       (list (build-alg-id id-sha512 #f))
                        (read-bytes-from-file "example.rkt")
-                       (read-bytes-from-file "data/freeware-user-cert.der")
-                       #f
-                       (make-set-of
-                        (build-signer-info (read-bytes-from-file "data/freeware-user-cert.der")
-                                           (build-alg-id id-sha512 #f) 'priv-key
-                                           (lambda (digest-alg data) (string->bytes/latin-1 "fff8889977ddde"))
-                                           (lambda (signature-alg priv-key digest) (string->bytes/latin-1 "fff8889977afea"))
-                                           (read-bytes-from-file "example.rkt")))
-                      ))])
+                       (list (read-bytes-from-file "data/freeware-user-cert.der"))
+                       null
+                       null
+                       #f                      
+                       (list 'priv-key)
+                       (lambda (digest-alg data) (string->bytes/latin-1 "fff8889977ddde"))
+                       (lambda (signature-alg priv-key digest) (string->bytes/latin-1 "fff8889977afea"))))])
+
+  
+                                                              
+                      
+  (printf "content data: \n")
+  (pretty-print content-data)
+  (printf "content data -> DER -> ASN.1: \n")
+  (pretty-print (bytes->asn1/DER ContentInfo (asn1->bytes/DER ContentInfo content-data))))
+
+(let ([content-data  (build-cms-content
+                      id-cms-signed-data
+                      (send+ (new signed-data-builder%)                      
+                             (add-content-bytes
+                              (read-bytes-from-file "example.rkt"))                      
+                             (add-signer-info)
+                             (add-digest-alg
+                              (build-alg-id id-sha512 #f))
+                             (add-cert-bytes
+                              (read-bytes-from-file "data/freeware-user-cert.der"))
+                             (add-private-key
+                              'priv-key)
+                             (add-calc-digest-proc
+                              (lambda (digest-alg data) (string->bytes/latin-1 "fff8889977ddde")))
+                             (add-sign-digest-proc
+                              (lambda (signature-alg priv-key digest) (string->bytes/latin-1 "fff8889977afea")))
+                             (end-def)
+                             (add-signer-info)
+                             (add-digest-alg
+                              (build-alg-id id-sha512 #f))
+                             (add-cert-bytes
+                              (read-bytes-from-file "data/freeware-user-cert_1.der"))
+                             (add-private-key
+                              'priv-key-2)
+                             (add-calc-digest-proc
+                              (lambda (digest-alg data) (string->bytes/latin-1 "fff8889977ddde")))
+                             (add-sign-digest-proc
+                              (lambda (signature-alg priv-key digest) (string->bytes/latin-1 "fff8889977afea")))
+                             (end-def)
+                             (build-asn1))
+                      )])
+
+  
+                                                              
+  (printf "==================================================================\n")
+  (printf "=========== CLASSES LOGIC ==================\n" )
+  (printf "==================================================================\n" )
   (printf "content data: \n")
   (pretty-print content-data)
   (printf "content data -> DER -> ASN.1: \n")
@@ -179,3 +438,4 @@
 
 (pretty-print (check-and-make-choice  recipient-info-choice (list 'pwri 'geheim)))
 ;;(pretty-print (check-and-make-choice  recipient-info-choice-def (list 'not-there 'geheim)))
+
